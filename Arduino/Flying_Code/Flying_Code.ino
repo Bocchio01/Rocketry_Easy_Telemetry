c05c243ccf99f8941340e3383996ebb81a1063ce
Bocchio01 <tommaso.bocchietti@gmail.com>
2023-07-16 23:22:16 +0200

    Arduino code updated with config possiblity

--- Arduino/Flying_Code/Flying_Code.ino
+++ Arduino/Flying_Code/Flying_Code.ino
@@ -3,107 +3,237 @@
 #include <SPI.h>
 #include <SD.h>
 
-#define seaLevelPressure_hPa 1013.25
+#define doBeep true
+
+#define defaultMode 0                   // Flight->0 / Test->1
+#define defaultloggingFrequency 2       // [Hz]
+#define defaultSeaLevelPressure 101325  // [Pa]
+
+#define positiveDelay 100
+#define negativeDelay 3000
+#define landOffDelay 1000
+#define MPU_addr 0x68
+#define SD_sckPin 10
+#define buzzerPin 6
 
 Adafruit_BMP085 bmp;
-const int MPU_addr = 0x68;  // I2C address of the MPU-6050
+char completeFileName[32], fileName[16];
+int16_t dataIndex = 0;
 int16_t AccX, AccY, AccZ;
 int32_t Pressure;
 float Temperature, Altitude;
-const int chipSelect = 10;
-const int ledPin = LED_BUILTIN;  // the number of the LED pin
+
+int mode = defaultMode;
+int loggingFrequency = defaultloggingFrequency;
+float seaLevelPressure = defaultSeaLevelPressure;
 
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  boolean BMP180_flag = false;
+  boolean GY_521_flag = false;
+  boolean SD_flag = false;
 
+  pinMode(buzzerPin, OUTPUT);
 
-  while (!Serial) {
-    ;  // wait for serial port to connect. Needed for native USB port only
-  }
-  if (!bmp.begin()) {
-    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-    while (1) {}
-  }
-
-  Wire.begin();
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
+  // Wait for Serial
+  while (!Serial) {}
   Serial.begin(9600);
 
-  Serial.print("Initializing SD card...");
+  // Wait for SD
+  do {
+    SD_flag = SD.begin(SD_sckPin);
+    if (SD_flag) {
+      Serial.println("SD module connected!");
+      doBuzzer(positiveDelay);
+    } else {
+      Serial.println("Could not find SD module!");
+      doBuzzer(negativeDelay);
+      delay(1000);
+    }
+  } while (!SD_flag);
+  delay(1000);
 
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1)
-      digitalWrite(ledPin, HIGH);
+  // Wait for BMP180
+  do {
+    BMP180_flag = bmp.begin();
+    if (BMP180_flag) {
+      Serial.println("BMP180 sensor connected!");
+      doBuzzer(positiveDelay);
+    } else {
+      Serial.println("Could not find BMP180 sensor!");
+      doBuzzer(negativeDelay);
+      delay(1000);
+    }
+  } while (!BMP180_flag);
+  delay(1000);
+
+  // Wait for GY_521
+  do {
+    Wire.begin();
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x6B);  // PWR_MGMT_1 register
+    Wire.write(0);     // set to zero (wakes up the MPU-6050)
+    GY_521_flag = Wire.endTransmission(true);
+    if (GY_521_flag == 0) {
+      Serial.println("GY-521 sensor connected!");
+      doBuzzer(positiveDelay);
+    } else {
+      Serial.println("Could not find GY-521 sensor!");
+      doBuzzer(negativeDelay);
+      delay(1000);
+    }
+  } while (GY_521_flag != 0);
+  delay(1000);
+
+
+  // Load configurations
+  readConfigFile();
+  switch (mode) {
+    case 0:
+      strcpy(fileName, "FLIGHT");
+      break;
+    case 1:
+      strcpy(fileName, "TEST");
+      break;
   }
-  Serial.println("card initialized.");
-  writeToFile("Temperature;Pressure;Altitude;AccX;AccY;AccZ");
+  delay(1000);
+
+
+  // Init new log file on SD card
+  initNewFile(fileName);
+  if (writeToFile("dataIndex;Time;Temperature;Pressure;Altitude;AccX;AccY;AccZ", completeFileName)) {
+    doBuzzer(positiveDelay);
+  } else {
+    doBuzzer(negativeDelay);
+  }
+  delay(1000);
+
+  // Setup complete
+  for (uint8_t i = 0; i < 5; i++) {
+    doBuzzer(100);
+    delay(50);
+  }
 }
 
 
 void loop() {
+  while (true) {
 
-  Temperature = bmp.readTemperature();
-  Pressure = bmp.readPressure();
-  Altitude = bmp.readAltitude(seaLevelPressure_hPa * 100);
+    // BMP180
+    Temperature = bmp.readTemperature();
+    Pressure = bmp.readPressure();
+    Altitude = bmp.readAltitude(seaLevelPressure);
 
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr, 14, true);  // request a total of 14 registers
+    // GY_521
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_addr, 14, true);  // request a total of 14 registers
 
-  AccX = Wire.read() << 8 | Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-  AccY = Wire.read() << 8 | Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AccZ = Wire.read() << 8 | Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+    AccX = Wire.read() << 8 | Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+    AccY = Wire.read() << 8 | Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+    AccZ = Wire.read() << 8 | Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
 
+    // Write data to file
+    char buffer[128], temperatureBuffer[5], altitudeBuffer[6];
+    dtostrf(Temperature, 5, 2, temperatureBuffer);
+    dtostrf(Altitude, 4, 2, altitudeBuffer);
+    snprintf(buffer, sizeof(buffer), "%d;%lu;%s;%ld;%s;%d;%d;%d", dataIndex, millis(), temperatureBuffer, Pressure, altitudeBuffer, AccX, AccY, AccZ);
+    writeToFile(buffer, completeFileName);
 
-  String dataString = "";
-  dataString += String(Temperature);
-  dataString += ";";
-  dataString += String(Pressure);
-  dataString += ";";
-  dataString += String(Altitude);
-  dataString += ";";
-  dataString += String(AccX);
-  dataString += ";";
-  dataString += String(AccY);
-  dataString += ";";
-  dataString += String(AccZ);
-
-  if (writeToFile(dataString))
-    digitalWrite(ledPin, LOW);
-  else
-    digitalWrite(ledPin, HIGH);
-
-  delay(1000);
+    dataIndex++;
+    delay(3000);
+  }
 }
 
 
-bool writeToFile(String dataString) {
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+void doBuzzer(uint16_t delayMs) {
+  if (doBeep) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(delayMs);
+    digitalWrite(buzzerPin, LOW);
+  }
+}
 
-  // if the file is available, write to it:
+
+void readConfigFile() {
+  File configFile;
+
+  configFile = SD.open("CONFIG.TXT");
+
+  if (configFile) {
+    while (configFile.available()) {
+      String line = configFile.readStringUntil('\n');
+
+      if (line.startsWith("#") || line.length() == 0) {
+        continue;
+      }
+
+      // Parse the key-value pair
+      int separatorIndex = line.indexOf('=');
+      if (separatorIndex != -1) {
+        String key = line.substring(0, separatorIndex);
+        String value = line.substring(separatorIndex + 1);
+
+        key.trim();
+        value.trim();
+
+        if (key.equals("mode")) {
+          mode = value.toInt();
+        } else if (key.equals("logging_frequency")) {
+          loggingFrequency = value.toInt();
+        } else if (key.equals("sea_level_pressure")) {
+          seaLevelPressure = value.toFloat();
+        }
+      }
+    }
+
+    configFile.close();
+  } else {
+    Serial.println("Error opening CONFIG.TXT file!");
+    Serial.println("Default values assigned.");
+  }
+
+  Serial.print("mode: ");
+  Serial.println(mode);
+  Serial.print("logging_frequency: ");
+  Serial.println(loggingFrequency);
+  Serial.print("sea_level_pressure: ");
+  Serial.println(seaLevelPressure);
+}
+
+
+void initNewFile(const char* fileName) {
+  int fileNumber = 0;
+
+  do {
+    snprintf(completeFileName, sizeof(completeFileName), "%d%s.CSV", fileNumber, fileName);
+    fileNumber++;
+  } while (SD.exists(completeFileName));
+
+  File dataFile = SD.open(completeFileName, FILE_WRITE);
   if (dataFile) {
+    Serial.print("File created: ");
+    Serial.println(completeFileName);
+    dataFile.close();
+  } else {
+    Serial.println("Error creating file!");
+    completeFileName[0] = '\0';  // Empty the completeFileName string
+  }
+
+}
+
+
+bool writeToFile(const char* dataString, const char* fileName) {
+  File dataFile = SD.open(fileName, FILE_WRITE);
+
+  if (dataFile) {
+    Serial.println(dataString);
     dataFile.println(dataString);
     dataFile.close();
-    // print to the serial port too:
-    Serial.println(dataString);
-  }
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening datalog.txt");
+    return true;
+  } else {
+    Serial.println("Error writing to " + String(fileName));
     return false;
   }
-
-  return true;
 }
